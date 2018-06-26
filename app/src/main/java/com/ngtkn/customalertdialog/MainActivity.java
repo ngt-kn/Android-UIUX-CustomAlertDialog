2a5e4db69eba807d15d5ecc81a9e9a8f18e358be
ngt kn <nagata.ken78@gmail.com>
2018-06-25 22:45:11 -0700

    add new drawables and layout for custom dialog

--- app/src/main/java/com/ngtkn/customalertdialog/MainActivity.java
+++ app/src/main/java/com/ngtkn/customalertdialog/MainActivity.java
@@ -1,13 +1,42 @@
 package com.ngtkn.customalertdialog;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dialog = new Dialog(this);
+    }
+
+    public void showDialog(View view) {
+        TextView textViewClose;
+        Button btnFollow;
+        // set the content of the dialog to custom dialog layout
+        dialog.setContentView(R.layout.custom_dialog);
+        textViewClose = dialog.findViewById(R.id.x);
+        btnFollow = dialog.findViewById(R.id.btn_follow);
+
+        textViewClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        // Set the background to transparent
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 }
